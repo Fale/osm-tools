a29618e06ea9a07d39286610fdd74c8ceb365685
fale <fale@server.(none)>
2010-11-14 23:51:20 +0100

    Add comments, add cleanup, re-add the download of the osm file

--- ita-initial.sh
+++ ita-initial.sh
@@ -1,15 +1,25 @@
-#wget http://download.geofabrik.de/osm/europe/italy.osm.bz2
-#bunzip2 italy.osm.bz2
+#!/bin/bash
 
+# Clean
+rm italy*
+
+# Download the planet
+wget http://download.geofabrik.de/osm/europe/italy.osm.bz2
+bunzip2 italy.osm.bz2
+
+# Get all the people from nodes
 grep 'node id' italy.osm | grep uid | awk -F'"' '{print $12" "$14}' > italy_nodes
 sort italy_nodes | uniq -c | sort -rn >italy_nodes_sorted
 
+# Get all the people from ways
 grep 'way id' italy.osm | grep uid | awk -F'"' '{print $8" "$10}' > italy_ways
 sort italy_ways | uniq -c | sort -rn > italy_ways_sorted
 
+# Get all the people from relations
 grep 'relation id' italy.osm | grep uid | awk -F'"' '{print $8" "$10}' > italy_relations
 sort italy_relations | uniq -c | sort -rn >italy_relations_sorted
 
+# Create a file with all previous data
 cat italy_nodes > italy_uids
 cat italy_ways >> italy_uids
 cat italy_relations >> italy_uids
