5d31d3c1a700c106950e6cbafb9cc656379917a1
fale <fale@server.(none)>
2010-11-16 23:07:00 +0100

    obviously the downloader should provide the files with the right names

--- downloader.sh
+++ downloader.sh
@@ -5,9 +5,9 @@ wget http://download.geofabrik.de/osm/$1/$2.osm.bz2
 bunzip2 $2.osm.bz2
 
 # Get all the people from nodes
-rm $2_uids
-rm $2_uids_sorted
-python parser.py $2 > $2_uids
-sort $2_uids | uniq -c | sort -rn > $2_uids_sorted
+rm $1_$2_uids
+rm $1_$2_uids_sorted
+python parser.py $2 > $1_$2_uids
+sort $1_$2_uids | uniq -c | sort -rn > $1_$2_uids_sorted
 
 rm $2.osm
