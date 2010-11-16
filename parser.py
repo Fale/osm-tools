e1b91afb0f477cae47b05000743a7335bffaf074
fale <fale@server.(none)>
2010-11-16 16:10:33 +0100

    Fix from Francesco Vezzoli to the parser.py

--- parser.py
+++ parser.py
@@ -6,11 +6,11 @@ f = open("italy.osm", "r")
 users = {}
 
 for line in f:
-   start = line.find("uid='")
+   start = line.find('uid="')
    if start != -1:
-       end = line.find("'",start+5)
+       end = line.find('"',start+5)
        uid = line[start+5:end]
-       start = line.find("user='")
-       end = line.find("'",start+6)
+       start = line.find('user="')
+       end = line.find('"',start+6)
        users[uid] = line[start+6:end]
        print line[start+6:end] + " " + uid
