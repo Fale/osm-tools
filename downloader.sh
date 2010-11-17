78e74e7b468825786f8929cd2a904b24a20d7b13
fale <fale@server.(none)>
2010-11-17 23:50:33 +0100

    Better not having some different lines during parsing time ;)

--- downloader.sh
+++ downloader.sh
@@ -9,7 +9,5 @@ rm $1_$2
 python parser.py $2 > $1_$2_uids
 sort $1_$2_uids | uniq -c | sort -rn > $1_$2
 
-sed -e "1i# File $2.osm: `stat -c '%y' $2.osm`" -i $1_$2
-
 rm $2.osm
 rm $1_$2_uids
--- state.sh
+++ state.sh
@@ -4,10 +4,12 @@
 python check_uids.py users_agreed.txt $1_$2 $2 > $1_$2_status
 python check_not_accepted.py users_agreed.txt $1_$2 > $1_$2_not
 
-sed -e "1i# File users_agreed.txt: `stat -c '%y' users_agreed.txt`" -i $1_$2
-
 # Better having results sorted
 sort -rn $1_$2_not > $1_$2_not_accepted
+
+# Add headers
+sed -e "1i# File users_agreed.txt: `stat -c '%y' users_agreed.txt`" -i $1_$2_not_accepted
+sed -e "1i# File $2.osm: `stat -c '%y' $1_$2`" -i $1_$2_not_accepted
 
 # Move data to a public web_server
 scp -P 65321 $1_$2_status chopr@repo.grimp.eu:/home/chopr/repo.grimp.eu/public_html/osm/
