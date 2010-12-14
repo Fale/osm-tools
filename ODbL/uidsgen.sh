81911bd125bdd7450b3f5d04df23bb353a6d62e7
fale <fale@server.(none)>
2010-12-14 23:07:21 +0100

    create a script to manage the extraction of the uids

---
+++ ODbL/uidsgen.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Uncompress the file
+bunzip2 -k $2.osm.bz2
+
+# Get all the people from nodes
+python parser.py $2 > $1_$2_uids
+sort $1_$2_uids | uniq -c | sort -rn > $1_$2
+
+rm $2.osm
+rm $1_$2_uids
